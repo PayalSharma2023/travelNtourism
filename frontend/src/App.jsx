8a58425bda223303e6a8517dcb652d2d8499f98b
Payal Sharma <payalsharmareal@gmail.com>
2025-01-27 14:18:52 +0530

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Destinations from "./components/Destinations";
+import Destinations from "./components/Destination";
 import Offers from "./components/Offers";
 import About from "./components/About";
 import Contact from "./components/Contact";
